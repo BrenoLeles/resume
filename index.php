69ed3811ffea7c26ecf62882ae738038fac77b37
Breno Leles <breno.leles@outlook.com>
2019-12-30 18:20:18 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Breno Leles</title>
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+</head>
+<body>
+
+
+<div class="container">
+    <div class="content">
+        <div class="row">
+            <div class="col-md-12">
+                <h1>teste</h1>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+<sript type="text/javascript" src="assets/js/jquery.min.js"></sript>
+<sript type="text/javascript" src="assets/js/core.js"></sript>
+<sript type="text/javascript" src="assets/js/bootstrap.min.js"></sript>
+
+</html>
